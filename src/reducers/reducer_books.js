95c871f5685fc8034499a0fa5670b4232838539f
Paul Simon Ongpin <paulsimon.ongpin@Pauls-MacBook-Pro.local>
2017-04-15 12:17:17 +0800

    Update BookList Component to BookList Container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    { title: 'Book 1' },
-    { title: 'Book 2' },
-    { title: 'Book 3' },
-    { title: 'Book 4' },
-    { title: 'Book 5' }
+    { title: 'Book One' },
+    { title: 'Book Two' },
+    { title: 'Book Three' },
+    { title: 'Book Four' },
+    { title: 'Book Five' }
   ];
 }
