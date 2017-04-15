eb10eaef600a183378cc32a1da63f007cd77debe
Paul Simon Ongpin <paulsimon.ongpin@Pauls-MacBook-Pro.local>
2017-04-15 16:34:15 +0800

    Update Books List to contain pages property.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    { title: 'Book One' },
-    { title: 'Book Two' },
-    { title: 'Book Three' },
-    { title: 'Book Four' },
-    { title: 'Book Five' }
+    { title: 'Book One', pages: 105 },
+    { title: 'Book Two', pages: 90 },
+    { title: 'Book Three', pages: 20 },
+    { title: 'Book Four', pages: 190 },
+    { title: 'Book Five', pages: 40 }
   ];
 }
